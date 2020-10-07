4cd4c4c58edcefe2bd5a44281830014f86648c98
Jonathan Landaverde <jonathan.landaverde18@gmail.com>
2020-10-07 12:37:28 -0600

    docs agregado

---
+++ docs/precache-manifest.67d609d8f3383ec15b9117d1adce2ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7320cdb5d8f5b68411e89677e1ad1eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daa038b020d5066b98b7",
+    "url": "/static/css/main.af643ab5.chunk.css"
+  },
+  {
+    "revision": "027ae5944804431f3db5",
+    "url": "/static/js/2.9f575afa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f575afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa038b020d5066b98b7",
+    "url": "/static/js/main.f4dbdd5d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
